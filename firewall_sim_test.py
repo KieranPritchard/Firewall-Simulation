e7966d89ad97f232f94405fde3cdc2070e0af57e
Kieran Pritchard <KieranPritchard06@gmail.com>
2025-09-15 20:23:35 +0100

    added functions

---
+++ firewall_sim_test.py
@@ -0,0 +1,20 @@
+def add_rules():
+    return
+
+def make_sender_ip_addresses():
+    return
+
+def make_receiver_ip_addresses():
+    return
+
+def make_packets():
+    return
+
+def check_packets():
+    return
+
+def main():
+    return
+
+if __name__ == "__main__":
+    main()
