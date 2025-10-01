cf3fd918f9827e6d91804f237343698b868aff02
Kieran Pritchard <KieranPritchard06@gmail.com>
2025-10-01 20:50:50 +0100

    make receviver packets function

--- firewall_simulation.cpp
+++ firewall_simulation.cpp
@@ -86,6 +86,59 @@ vector<Packet> make_sender_packets(){
     }
 }
 
+vector<Packet> make_receiver_packets() {
+    vector<Packet> packets;
+    int packet_count = rand() % 301 + 200; // 200–500
+
+    vector<int> common_ports = {80, 443, 53, 25, 110, 22, rand() % 255 + 1};
+    vector<int> weights       = {40, 40, 5, 5, 5, 5, 5};
+
+    for (int i = 0; i < packet_count; i++) {
+        vector<int> all_ports = common_ports;
+        vector<int> all_weights = weights;
+
+        for (int j = 0; j < 10; j++) {
+            all_ports.push_back(rand() % (65535 - 1024 + 1) + 1024);
+            all_weights.push_back(1);
+        }
+
+        int total_weight = 0;
+        for (int w : all_weights) total_weight += w;
+        int r = rand() % total_weight;
+        int chosen_port = -1;
+        for (size_t k = 0; k < all_ports.size(); k++) {
+            r -= all_weights[k];
+            if (r < 0) {
+                chosen_port = all_ports[k];
+                break;
+            }
+        }
+
+        Packet new_packet;
+        new_packet.ip = make_ip_range(false); // public
+        new_packet.port = rand() % 500 + 1;   // 1–500
+        new_packet.proto = (rand() % 2 == 0 ? "tcp" : "udp");
+
+        packets.push_back(new_packet);
+    }
+
+    // Vulnerable ports list
+    vector<int> vulnerable_ports = {
+        20,21,22,23,25,53,67,68,69,80,443,110,143,
+        137,138,139,445,123,161,5060,5061,554,3306,
+        5432,1433,27017,3389,5900,5000,8000,8080,8443
+    };
+
+    // Randomly overwrite some packets with vulnerable ports
+    for (int i = 0; i < (packet_count / 5) * 2; i++) {
+        int random_index = rand() % packet_count;
+        int random_port = vulnerable_ports[rand() % vulnerable_ports.size()];
+        packets[random_index].port = random_port;
+    }
+
+    return packets;
+}
+
 int main(){
     // This seeds the random number so it is completely different each time
     srand(time(0));
