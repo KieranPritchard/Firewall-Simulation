a43e288f04ad39654f02b4e5b8bc04a66641c4d8
Kieran Pritchard <KieranPritchard06@gmail.com>
2025-10-01 20:32:09 +0100

    added make sender packets function

--- firewall_simulation.cpp
+++ firewall_simulation.cpp
@@ -8,14 +8,11 @@
 
 using namespace std;
 
-// This seeds the random number so it is completely different each time
-srand(time(0));
-
 // Structs for certain elements
 struct Packet {
     string ip;
     int port;
-    string prot; // "tcp" or "udp"
+    string proto; // "tcp" or "udp"
 };
 
 struct Rule{
@@ -50,4 +47,46 @@ string make_ip_range(bool range_type){
         string public_ip = to_string(rand() % 256) + "." + to_string(rand() % 256) + "." + to_string(rand() % 256) + "." + to_string(rand() % 256);
         return public_ip;
     }
+}
+
+vector<Packet> make_sender_packets(){
+    vector<Packet> packets;
+    int packet_count = rand() % 301 + 200;
+
+    vector<int> common_ports = {80, 443, 53, 25, 110, 22, rand() % 255 + 1};
+    vector<int> weights = {40, 40, 5, 5, 5, 5, 5};
+
+    for(int i = 0; i < packet_count; i++){
+        // Adds 10 random ports 
+        vector<int> all_ports = common_ports;
+        vector<int> all_weights = weights;
+        for(int j = 0; j < 10; j++) {
+            all_ports.push_back(rand() % (65535 - 1024 + 1) + 1024);
+            all_weights.push_back(1);
+        }
+        // Weighted random choice
+        int total_weight = 0;
+        for (int w : all_weights) total_weight += w;
+        int r = rand() % total_weight;
+        int chosen_port = -1;
+        for (size_t k = 0; k < all_ports.size(); k++) {
+            r -= all_weights[k];
+            if (r < 0) {
+                chosen_port = all_ports[k];
+                break;
+            }
+        }
+
+        Packet new_packet;
+        new_packet.ip = make_ip_range(true); // to make the ip private
+        new_packet.port = chosen_port;
+        new_packet.proto = (rand() % 2 == 0 ? "tcp" : "udp");
+
+        packets.push_back(new_packet);
+    }
+}
+
+int main(){
+    // This seeds the random number so it is completely different each time
+    srand(time(0));
 }
